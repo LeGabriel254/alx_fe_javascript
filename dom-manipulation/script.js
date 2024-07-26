bea63b35b23c7034727c5c0db0265646594be46d
Leon Gabriel <g73346920@gmail.com>
2024-07-26 14:20:33 +0300

    update changes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -58,8 +58,13 @@ exportButton.addEventListener('click', function() {
 });
 
 
-//Implemented JSON Import
-const Div = document.createElement(Div);
-Div.innerHTML =  `<input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />`
-// Add the form to the page
-document.body.appendChild(Div);
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
