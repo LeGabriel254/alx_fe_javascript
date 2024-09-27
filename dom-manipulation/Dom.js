8bca16bf6753c012288ed2accbd7df3dcd2f13dd
Leon Gabriel <g73346920@gmail.com>
2024-09-27 11:01:58 +0300

    Modified

--- dom-manipulation/Dom.js
+++ dom-manipulation/Dom.js
@@ -1,7 +1,13 @@
-var element =  document.createElement('p')
+var element =  document.createElement('p') 
 element.textContent='Its me Leon'
-document.body.appendChild(element)
+document.body.appendChild(element)// show into the document
 
 document.getElementById('pyr').textContent= 'Hello World'
  var age = prompt('How old are you');
+
+ // arrays and objects
+ var myName = {}
+  John = { firstName:"Leon", lastName:"Gabriel", fullName:"Leon Gabriel"}
+
+  console.log(John.fullName)
  
