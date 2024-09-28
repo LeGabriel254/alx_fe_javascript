ea6dbd5eee70ea86b5c81af401d510406b80070f
Leon Gabriel <g73346920@gmail.com>
2024-09-29 00:55:28 +0300

    for loop statement used

--- dom-manipulation/Dom.js
+++ dom-manipulation/Dom.js
@@ -1,24 +1,45 @@
-var element =  document.createElement('p') 
-element.textContent='Its me Leon'
-document.body.appendChild(element)// show into the document
+// var element = document.createElement('p')
+// element.textContent = 'Its me Leon'
+// document.body.appendChild(element)// show into the document
 
-document.getElementById('pyr').textContent= 'Hello World'
- var age = prompt('How old are you');
- var name = prompt("Whats your name")
+// document.getElementById('pyr').textContent = 'Hello World'
+// var age = prompt('How old are you');
+// var name = prompt("Whats your name")
 
- // Get the current year using new Date().getFullYear()
-let currentYear = new Date().getFullYear();
+// // Get the current year using new Date().getFullYear()
+// let currentYear = new Date().getFullYear();
 
-// Calculate the year of birth by subtracting userAge from the current year
-let yearOfBirth = currentYear - userAge;
+// // Calculate the year of birth by subtracting userAge from the current year
+// let yearOfBirth = currentYear - userAge;
 
-// Display the result to the user using an alert
-alert(`Hello, ${userName}! You were born in the year ${yearOfBirth}.`);
+// // Display the result to the user using an alert
+// alert(`Hello, ${userName}! You were born in the year ${yearOfBirth}.`);
 
-//  // arrays and objects
-//  let name = "Leon"
+// //  // arrays and objects
+// //  let name = "Leon"
 
-//  console.log()
+// //  console.log()
 
 
-//if statement
+// //if statement
+function flatten(array) {
+  const result = []
+  function flattenHelp(nestArry) {
+    for (let i = 0; i < nestArry.length; i++) {
+      if (Array.isArray(nestArry[i])) {
+        flattenHelp(nestArry[i]);
+      } else {
+        result.push(nestArry[i]);
+      }
+    }
+
+  }
+  flattenHelp(array);
+  return result;
+}
+
+const nestArry = [[1, 2, [3]], 4];
+const result = flatten(nestArry);
+
+console.log(result);
+
