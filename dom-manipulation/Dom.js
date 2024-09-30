17a13a32f6b24ad1dddf54949031a899a20f3cbf
Leon Gabriel <g73346920@gmail.com>
2024-09-30 16:47:06 +0300

    go to google function

--- dom-manipulation/Dom.js
+++ dom-manipulation/Dom.js
@@ -1,4 +1,4 @@
-// var element = document.createElement('p')
+// let element = document.createElement('p')
 // element.textContent = 'Its me Leon'
 // document.body.appendChild(element)// show into the document
 
@@ -6,40 +6,41 @@
 // var age = prompt('How old are you');
 // var name = prompt("Whats your name")
 
-// // Get the current year using new Date().getFullYear()
+// // // Get the current year using new Date().getFullYear()
 // let currentYear = new Date().getFullYear();
 
-// // Calculate the year of birth by subtracting userAge from the current year
+// // // Calculate the year of birth by subtracting userAge from the current year
 // let yearOfBirth = currentYear - userAge;
 
-// // Display the result to the user using an alert
+// // // Display the result to the user using an alert
 // alert(`Hello, ${userName}! You were born in the year ${yearOfBirth}.`);
 
-// //  // arrays and objects
-// //  let name = "Leon"
+// // //  // arrays and objects
 
-// //  console.log()
+// // //if statement
+// function flatten(array) {
+//   const result = []
+//   function flattenHelp(nestArry) {
+//     for (let i = 0; i < nestArry.length; i++) {
+//       if (Array.isArray(nestArry[i])) {
+//         flattenHelp(nestArry[i]);
+//       } else {
+//         result.push(nestArry[i]);
+//       }
+//     }
 
+//   }
+//   flattenHelp(array);
+//   return result;
+// }
 
-// //if statement
-function flatten(array) {
-  const result = []
-  function flattenHelp(nestArry) {
-    for (let i = 0; i < nestArry.length; i++) {
-      if (Array.isArray(nestArry[i])) {
-        flattenHelp(nestArry[i]);
-      } else {
-        result.push(nestArry[i]);
-      }
-    }
+// const nestArry = [[1, 2, [3]], 4];
+// const result = flatten(nestArry);
 
-  }
-  flattenHelp(array);
-  return result;
+// console.log(result);
+function go_to_google() {
+if ( confirm('Go to Google?') ) {
+  document.location = 
+ 'http://www.google.com/';
+ }
 }
-
-const nestArry = [[1, 2, [3]], 4];
-const result = flatten(nestArry);
-
-console.log(result);
-
